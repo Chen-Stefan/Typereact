286eb2988ff2980cfd24ad71dd946cfcf7f7770a
Stefan Chen <zchen158@my.bcit.ca>
2022-07-12 14:33:11 -0700

    Use children props with ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { ReactElement, ReactNode } from "react";
+import "./App.css";
 
-// Conventional props 
-function Header({title}: {title: string}) {
-  return(
-    <h1>{title}</h1>
-  )
+// Conventional props
+function Header({ title }: { title: string }) {
+  return <h1>{title}</h1>;
+}
+
+function HeaderWithContent({ children }: { children: ReactNode }): ReactElement {
+  return <h1>{children}</h1>;
 }
 
 function App() {
   return (
     <div>
-      <Header title={'hello'}></Header>
+      <Header title={"hello"}></Header>
+      <HeaderWithContent><strong>Hi</strong></HeaderWithContent>
     </div>
   );
 }
