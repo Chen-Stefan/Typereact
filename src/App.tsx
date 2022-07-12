65bf00c767cf56984cba74ff6fdebe70a1d13871
Stefan Chen <zchen158@my.bcit.ca>
2022-07-11 20:33:47 -0700

    Initialize a react app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Hello World' color='red'/>
+    </div>
+  );
+}
+
+export default App;
