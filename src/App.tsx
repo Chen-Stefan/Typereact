47df803da0db2f058b4c368767934ddfb0557b7f
Stefan Chen <zchen158@my.bcit.ca>
2022-07-12 14:45:43 -0700

    Use default props

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,30 @@ import React, { ReactElement, ReactNode } from "react";
 import "./App.css";
 
 // Conventional props
-function Header({ title }: { title: string }) {
+function Heading({ title }: { title: string }) {
   return <h1>{title}</h1>;
 }
 
-function HeaderWithContent({ children }: { children: ReactNode }): ReactElement {
+function HeadingWithContent({ children }: { children: ReactNode }): ReactElement {
   return <h1>{children}</h1>;
 }
+
+const defaulfContainerProps = {
+  heading: <strong>My heading</strong>
+}
+
+function Container({heading, children} : {children: ReactNode} & typeof defaulfContainerProps): ReactElement {
+  return <div><h1>{heading}</h1>{children}</div>
+}
+// Children 指的是两个tag中间夹的内容
+Container.defaultProps = defaulfContainerProps
 
 function App() {
   return (
     <div>
-      <Header title={"hello"}></Header>
-      <HeaderWithContent><strong>Hi</strong></HeaderWithContent>
+      <Heading title={"hello"}></Heading>
+      <HeadingWithContent><strong>Hi</strong></HeadingWithContent>
+      <Container>Food</Container>  
     </div>
   );
 }
